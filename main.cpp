bacb390731ca474a2fef11db65366d1ebee970b8
Jack Arney <arney.jh@gmail.com>
2020-10-25 17:27:05 +1030

    Started debugging. Data points vector is not being passed around correct.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 #include <time.h>
 #include <vector>
 
+#ifdef DEBUG
+	#define LOG(x) std::cout << x << std::endl;
+#else
+	#define LOG(x) 
+#endif // DEBUG
+
+
 #define correctNumParams 3
 #define numClusterMinimum 2
 #define numDataPoints 240
@@ -24,16 +31,27 @@ struct Point {
 void kmeans(std::vector<Point>*, size_t);
 void identify(std::vector<Point>*, std::vector<Point>*);
 float OptEucDist(Point*, Point*);
+void computeNewClusters(std::vector<Point>*, std::vector<Point>*);
 void grabClusterIDs(std::vector<Point>*, std::vector<int>*);
 bool differentClusterIDs(std::vector<Point>*, std::vector<int>*);
+void printVecPoints(std::vector<Point>*);
 
 
 int main(int argc, char *argv[]) {
 
-	// function guard
-	if(argc != correctNumParams) return -1;
+	// function guards
+	if(argc != correctNumParams) {
+
+		std::cout << "Incorrect number of arguments" << std::endl;
+		return -1;
+	}
+
 	int numClusters = atoi(argv[2]);
-	if(numClusters < numClusterMinimum) return -1;
+	if(numClusters < numClusterMinimum) {
+		
+		std::cout << "Invalid number of clusters" << std::endl;
+		return -1;
+	}
 
 	// open file given by command line
 	std::fstream dataFile;
@@ -92,6 +110,8 @@ int main(int argc, char *argv[]) {
 
 void kmeans(std::vector<Point>* data, size_t numClusters) {
 
+	LOG("Starting kmeans");
+
 	// randomise cluster selection from the given number of points
 	srand(time(NULL));
 	size_t randomIndex = 0;
@@ -105,11 +125,15 @@ void kmeans(std::vector<Point>* data, size_t numClusters) {
 	// buffer for checking whether cluster IDs change
 	std::vector<int> clusterIDbuffer(data->size());
 	identify(data, &clusterPoints);
+
+	printVecPoints(data);
+
 	grabClusterIDs(data, &clusterIDbuffer);
 	while(differentClusterIDs(data, &clusterIDbuffer)) {
 
-		// compute new clusters() ***TODO***
+		LOG("New iteration of kmeans");
 
+		computeNewClusters(data, &clusterPoints);
 		identify(data, &clusterPoints);
 		grabClusterIDs(data, &clusterIDbuffer);
 	}
@@ -118,30 +142,82 @@ void kmeans(std::vector<Point>* data, size_t numClusters) {
 
 float OptEucDist(Point* point1, Point* point2) {
 
+	LOG("OptEucDist");
+	std::cout << "Point1: (" << point1->x << "," << point1->y << "," << point1->z << ")" << std::endl;
+	std::cout << "Point2: (" << point2->x << "," << point2->y << "," << point2->z << ")" << std::endl;
 	return pow(point1->x - point2->x, 2.0) + pow(point1->y - point2->y, 2.0) + pow(point1->z - point2->z, 2.0);
 
 }
 
+
+void computeNewClusters(std::vector<Point>* data, std::vector<Point>* clusterPoints) {
+
+	LOG("Starting computeNewClusters");
+
+	std::vector<int> numOfPointsInCluster(clusterPoints->size(), 0);
+
+	// sum ordinates of each points that are part of the cluster 
+	for(size_t i = 0; i < data->size(); i++) {
+
+		clusterPoints->at(data->at(i).clusterID).x += data->at(i).x;
+		clusterPoints->at(data->at(i).clusterID).y += data->at(i).y;
+		clusterPoints->at(data->at(i).clusterID).z += data->at(i).z;
+		numOfPointsInCluster.at(data->at(i).clusterID)++;
+
+	}
+
+	// calculate average using the number of points within the cluster
+	for(size_t i = 0; i < clusterPoints->size(); i++) {
+
+		clusterPoints->at(i).x /= numOfPointsInCluster.at(i);
+		clusterPoints->at(i).y /= numOfPointsInCluster.at(i);
+		clusterPoints->at(i).z /= numOfPointsInCluster.at(i);
+	}
+
+}
+
+
 void identify(std::vector<Point>* data, std::vector<Point>* clusterPoints) {
+
+	LOG("New identify call");
 
 	float minDist = std::numeric_limits<float>::max();
 	float tempDist = 0;
 	int closestClusterID = -1;
 	for(size_t i = 0; i < data->size(); i++) {
 
+		LOG("Point identify");
+		std::cout << "(" << data->at(i).x << "," << data->at(i).y << ",";
+		std::cout << data->at(i).z << ")" << std::endl;
+
 		for(size_t j = 0; j < clusterPoints->size(); j++) {
 
+			std::cout << "Cluster Point: " << "(" << clusterPoints->at(j).x;
+			std::cout << "," << clusterPoints->at(j).y << ",";
+			std::cout << clusterPoints->at(j).z << std::endl;
+
 			// check against all available clusters and find the closest
-			tempDist = OptEucDist(&data->at(i), &clusterPoints->at(j));
+			tempDist = OptEucDist(&(data->at(i)), &clusterPoints->at(j));
+			std::cout << "Distance between: " << tempDist << std::endl;
+
 			if(tempDist < minDist) {
 
 				closestClusterID = j;
 				minDist = tempDist;
 			}
 
+		#ifdef DEBUG
+			LOG("HALTED");
+			std::getc(stdin);
+		#endif // DEBUG
+
 		}
 
 		data->at(i).clusterID = closestClusterID;
+		#ifdef DEBUG
+			LOG("HALTED");
+			std::getc(stdin);
+		#endif // DEBUG
 
 	}
 }
@@ -150,6 +226,8 @@ void identify(std::vector<Point>* data, std::vector<Point>* clusterPoints) {
 
 void grabClusterIDs(std::vector<Point>* data, std::vector<int>* clusterIDbuffer) {
 
+	LOG("Grabbing cluster IDs");
+
 	for(size_t i = 0; i < data->size(); i++)
 		clusterIDbuffer->at(i) = data->at(i).clusterID;
 }
@@ -157,12 +235,31 @@ void grabClusterIDs(std::vector<Point>* data, std::vector<int>* clusterIDbuffer)
 
 bool differentClusterIDs(std::vector<Point>* data, std::vector<int>* clusterIDbuffer) {
 
+	LOG("Determined if IDs have changed");
+
 	bool isDifferent = false;
 	for(size_t i = 0; i < data->size(); i++) {
 
-		isDifferent |= (data->at(i).clusterID == clusterIDbuffer->at(i));
+		isDifferent |= (data->at(i).clusterID != clusterIDbuffer->at(i));
 
 	}
 
+	#ifdef DEBUG
+		LOG("HALTED");
+		std::getc(stdin);
+	#endif // DEBUG
+
 	return isDifferent;
+}
+
+
+void printVecPoints(std::vector<Point>* input) {
+
+	for(size_t i = 0; i < input->size(); i++) {
+		
+		std::cout << "Point: (" << input->at(i).x;
+	   	std::cout << "," << input->at(i).y << ","; 
+		std::cout << input->at(i).z << ")";
+		std::cout << " clusterID: " << input->at(i).clusterID << std::endl;
+	}
 }
--- makefile
+++ makefile
@@ -1,8 +1,15 @@
 CC = g++
 CFLAGS = -Wall -Werror -Wextra -pedantic
+DBGCFLAGS = -g -DDEBUG
 
-.PHONY: clean
+EXE = main
 
+.PHONY: all debug clean
+
+all: main
+
+debug: main.cpp
+	$(CC) $(CFLAGS) $(DBGCFLAGS) -o $@ $^
 
 main: main.cpp
 	$(CC) $(CFLAGS) -o $@ $^ 
